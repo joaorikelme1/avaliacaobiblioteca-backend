d6744d82973e37f384bfc8349372003db65a0cbb
João Rikelme <joao.rikelme.jr1@gmail.com>
2026-09-03 20:30:42 -0300

    fix: corrige frontend e backend usam portas diferentes (OS 001)

--- src/main/java/com/senac/biblioteca/controller/EmprestimoController.java
+++ src/main/java/com/senac/biblioteca/controller/EmprestimoController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/emprestimos")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:5173")
 public class EmprestimoController {
 
     private final EmprestimoService emprestimoService;
